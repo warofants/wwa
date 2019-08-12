aef67bc31306775a8efb10564e5ff2b51e7d2147
ehwhoami <zyunlam@gmail.com>
2019-08-12 17:58:34 -0500

    Add constants so it is easier

--- src/main/java/world/war/ants/WorldWarAnts.java
+++ src/main/java/world/war/ants/WorldWarAnts.java
@@ -5,28 +5,32 @@ import world.war.ants.gui.TextMenu;
 
 public class WorldWarAnts {
     public static void main(String[] args) {
+        final String NEW_GAME = "New Game";
+        final String ABOUT = "About";
+        final String HELP = "Help";
+        final String EXIT = "Exit";
         TextMenu menu = new TextMenu();
-        menu.addOption("New Game");
-        menu.addOption("About");
-        menu.addOption("Help");
-        menu.addOption("Exit");
+        menu.addOption(NEW_GAME);
+        menu.addOption(ABOUT);
+        menu.addOption(HELP);
+        menu.addOption(EXIT);
         
         System.out.println("World War of Ants");
         System.out.println();
         System.out.println("Main Menu");
         //I KNOW THIS IS NOT GOOD BUT IDK HOW ELSE TO DO IT!
         int selection = menu.display();
-        switch(selection) {
-            case 1:
+        switch(menu.getOption(selection)) {
+            case NEW_GAME:
                 //New game
                 break;
-            case 2:
+            case ABOUT:
                 //About
                 break;
-            case 3:
+            case HELP:
                 //Helo
                 break;
-            case 4:
+            case EXIT:
                 //Exit
                 System.exit(0);
                 break;
