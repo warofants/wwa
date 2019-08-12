1e719b6eb7c84e369a2d63011195e8dbaf1020fe
Jannik143 <54024969+Jannik143@users.noreply.github.com>
2019-08-12 20:09:22 +0200

    Add files via upload

--- src/menu.java
+++ src/menu.java
@@ -4,9 +4,7 @@ import java.io.*;
 
 public class menu {
  
-   
   
-
     public static void main(String[] args) throws IOException {
     
     Scanner keyboard = new Scanner(System.in);
@@ -15,36 +13,32 @@ public class menu {
     System.out.println(" ###################\n#                   #\n# World War of Ants #\n#                   # \n ###################  \n ");
     
     System.out.println(" _________\n 1) Start \n _________\n 2) Load\n _________\n 3) Exit \n _________\n\n");
-    
-    int text = keyboard.nextInt(); 
  
-    if (text == 1 )
+ 
+    int text = keyboard.next();
     
-      {
-                                 
-      System.out.println("Game is loading...");
-     
-      }
-     
-
-    if (text == 2)
-    {
+       
+     switch (text)
     
-      System.out.println("Which state do you want to load?");
-     
-      }
-     
+     {
     
-    
-    if (text == 3)
-    {
+      case 1:
+        System.out.println("Game is loading...");
+         break;  
+      case 2:
+        System.out.println("Which state do you want to load?");
+        break;
+      case 3:
+        System.out.println("Which state do you want to load?");
+        break;
       
-      System.out.println("Which state do you want to load?");
-     
-      }
-     
+      default:
+      System.out.println("Wrong input!");   
 
-    } 
+       }   //end of switch
 
 
-  } //end of wwa 
+
+  }  //end of static void main
+  
+  }  //end of wwa
