0c88b696b406eb974199a735b8ca820209060e7f
nylanderdev <rasmus.s.nylander@gmail.com>
2019-08-13 22:02:52 +0200

    Add WorldTile + methods related to it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,14 @@
 package main.java;
 
 import main.java.logic.Game;
-import main.java.logic.World;
-import main.java.logic.Entity;
 
 public class Main {
     public static void main(String[] args) {
-        World myworld = new World(5, 5);
-	
+        Game game = new Game(4);
+
+        while (!game.isFinished()) {
+            game.tick();
+            // Render goes here
+        }
     }
 }
