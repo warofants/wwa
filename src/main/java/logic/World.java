56173602d20f392213114fcb0787e0a3146b6fa2
nylanderdev <rasmus.s.nylander@gmail.com>
2019-08-13 20:55:43 +0200

    Add WorldTile. Add methods for Entity, World

--- src/main/java/logic/World.java
+++ src/main/java/logic/World.java
@@ -1,15 +1,49 @@
 package main.java.logic;
 
+import java.util.List;
 
 /**
  * Class that handles all location specific data (ie unit collisions)
  */
 public class World {
-    public World() {
+    private List<WorldTile> worldMap;
+    private int width;
+    private int height;
 
+    public World(int width, int height) {
+        worldMap = new ArrayList<WorldTile>();
+	for (int i = 0; i < width * height; i++) {
+            worldMap[i] = new WorldTile();
+	}
+    }
+    
+    
+
+    public void addQueen(int x, int y, int playerID) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("World::addQueen is unimplemented!");
+    }
+    
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+     
+    public WorldTile getTile(int x, int y) {
+        return new WorldTile(worldMap[x + y * width]);
     }
 
-    public void addQueen(int x, int y, int playerID) {
 
+    public void setWidth(int newWidth) {
+        width = newWidth;
+    }
+
+    public void setHeight(int newHeight) {
+        height = newHeight;
+    }
+
+    public void setTile(int x, int y, WorldTile tile) {
+        worldMap[x + y * width] = tile;
     }
 }
