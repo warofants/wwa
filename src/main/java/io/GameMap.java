d55b29b8b247f23f32d8325e0e2192aee865372c
TrainingAfternoon <49281515+TrainingAfternoon@users.noreply.github.com>
2019-08-14 15:21:17 -0500

    Update GameMap.java

--- src/main/java/io/GameMap.java
+++ src/main/java/io/GameMap.java
@@ -1,5 +1,4 @@
 import java.util.List;
-import main.java.logic.Entity;
 //TODO: Allow scrolling
 //TODO: Construction of GameMap
 /**
@@ -10,29 +9,13 @@ class GameMap {
     private short numRows, numCols;
     private int[][] fullMap;
     private int[][] userView;
-    private List<Entity> entities;
-
+    
     public GameMap() {}
-
-    /**
-     * Add an entity to the internal list of entities
-     * @param entity
-     */
-    public void addEntity(Entity entity) {
-        entities.add(entity);
-    }
 
     /**
      * Method that prints out the game map
      */
     public void printMap() {
-
-    }
-
-    /**
-     * Method that places entites from entities list on the map
-     */
-    private void placeEntities() {
 
     }
 
