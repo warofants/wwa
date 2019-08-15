5b7d9aaed4ea78c647ca00e389b8cc37ddbe8731
Plat <plat251@gmail.com>
2019-08-15 23:27:29 +0300

    Add RandGen for project-centric RNG functionality

---
+++ src/main/java/com/worldwarofants/game/util/RandomGenerator.java
@@ -0,0 +1,94 @@
+package com.worldwarofants.game.util;
+
+import java.util.Random;
+
+/**
+* Utility class that provides an abstraction of Java's Random class.
+* Its goal is to provide developers with a simple way to work with
+* random number generation, as well as allow for precise tweaking
+* of exact methods used.
+* Please make sure to generate your random numbers within the project
+* exclusively using this package.
+* @author Plat251
+* @version 1.0
+* @since 2019-08-15 
+*/
+
+public class RandomGenerator extends java.util.Random {
+    private static final long serialVersionUID = -5378246343912387722L;
+    private static final String VALID_LONG_REGEX = "-?\\d+(\\.\\d+)?";
+
+    private Random generator;
+    private long seed;
+    private String seedString;
+
+    /**
+     * Instantiate a RandGen generator with no seed
+     */
+    public RandomGenerator() {
+        this.generator = new Random(); // Create a generator
+        this.seed = generator.nextLong(); // Ask it to generate a seed for itself
+        this.seedString = Long.toString(this.seed); // Save it to seedString as well
+        this.generator.setSeed(seed); // And set its seed to what it produced
+
+    }
+
+     /**
+     * Instantiate a RandGen generator with a String type object.
+     * If the string can be parsed as a long, it will be interpreted as a long.
+     */
+    public RandomGenerator(String seedString) {
+        if (seedString.matches(VALID_LONG_REGEX)) {
+            this.seed = Long.parseLong(seedString);
+            this.seedString = Long.toString(this.seed);
+        } else {
+            this.seed = seedString.hashCode();
+            this.seedString = seedString;
+        }
+        this.generator = new Random(seed);
+    }
+
+    /**
+     * @return long Returns the numeric seed used.
+     */
+    public long getSeed() {
+        return this.seed;
+    }
+
+    /**
+     * @return String Returns the string seed used. May be same as non-string seed.
+     */
+    public String getSeedString() {
+        return this.seedString;
+    }
+
+    /** 
+    * Generate an int in range
+    * If two ints are provided, use them to define a range
+    * If only one int is provided, range starts at 0.
+    * Range is START to END - 1.
+    * @return int Returns a random integer.
+    */
+    public int range(int start, int end) {
+        if (start > end) {
+            int swap = end;
+            end = start;
+            start = swap;
+        }
+        int randomNumber = this.generator.nextInt(end - start);
+        randomNumber += start;
+        return randomNumber;
+    }
+
+    /**
+    * Generate an int in range
+    * If two ints are provided, use them to define a range
+    * If only one int is provided, range starts at 0.
+    * Range is START to END - 1.
+    * @return int Returns a random integer.
+    */
+    public int range(int end) {
+        int randomNumber = this.generator.nextInt(end);
+        return randomNumber;
+    }
+}
