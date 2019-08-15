d1fa0de0ed9c6706cade6e9c56dea02e3bf1abeb
Orhan <lemikistu@gmail.com>
2019-08-15 20:42:39 +0100

    Added Entity Class with all fields we defined in UML , created Ant,Wasp and Spider classes which inherited from entity class, created QueenAnt and WorkerAnt class which inherited from Ant class. Remove game and player.java as those didnt had need. Removed getters  and setters as these were not needed in inheritance environment.

--- src/main/java/com/worldwarofants/game/Main.java
+++ src/main/java/com/worldwarofants/game/Main.java
@@ -2,12 +2,6 @@ package com.worldwarofants.game;
 
 public class Main {
     public static void main(String[] args) {
-        //Game game = new Game(4);
-
-        //while (!game.isFinished()) {
-         //   game.tick();
-            // Render goes here
-      //  }
 
         System.out.println("Worked");
     }
