628a705af300eb43fdc4fd634d0075b42bfbc817
doubleapple123 <leonychen123@gmail.com>
2019-08-17 22:28:33 -0700

    update

--- src/main/java/com/worldwarofants/game/logic/structures/AbstractStructure.java
+++ src/main/java/com/worldwarofants/game/logic/structures/AbstractStructure.java
@@ -2,11 +2,37 @@ package com.worldwarofants.game.logic.structures;
 
 public abstract class AbstractStructure{
 	private float hp;
-	private boolean storage;
+	private float resourceAmount; //how much resource created per tick
+	private float storageAmount; //how much max storage structure provides
+	private boolean isUpgradable; //if current structure is upgradable
+	private String resourceType; //what will the structure produce
+	private int xCoor, yCoor; //(x,y) coordinates
+	private int id; //unique strucutre id
 
-	//(x,y) coordinates
-	private int xCoor, yCoor;
-	private int id;
+	//getters and setters
+	public float getResourceAmount(){
+		return resourceAmount;
+	}
+
+	public void setResourceAmount(float resourceAmount){
+		this.resourceAmount = resourceAmount;
+	}
+
+	public String getResourceType(){
+		return resourceType;
+	}
+
+	public void setResourceType(String resourceType){
+		this.resourceType = resourceType;
+	}
+
+	public void setUpgradable(boolean upgradable){
+		this.isUpgradable = upgradable;
+	}
+
+	public boolean isUpgradable(){
+		return isUpgradable;
+	}
 
 	public float getHp(){
 		return hp;
--- src/main/java/com/worldwarofants/game/logic/structures/Test.java
+++
@@ -1,7 +0,0 @@
-package com.worldwarofants.game.logic.structures;
-
-public class Test{
-	public static void main(String[] args){
-
-	}
-}
