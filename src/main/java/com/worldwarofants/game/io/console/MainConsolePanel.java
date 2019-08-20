1897c2fda7b93a8906e624a8cfb9cde1b9613ba4
TrainingAfternoon <samkeyser1@gmail.com>
2019-08-19 21:45:15 -0500

    Updated Console, GameState classes

--- src/main/java/com/worldwarofants/game/io/console/MainConsolePanel.java
+++ src/main/java/com/worldwarofants/game/io/console/MainConsolePanel.java
@@ -2,23 +2,28 @@ package com.worldwarofants.game.io.console;
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+/**
+ * A class that acts as the content pane for
+ * MainConsoleFrame and displays the game state to the user
+ */
 class MainConsolePanel extends JPanel {
     private JLabel header;
-    private JTextArea gameText;
+    private JTextPane gameText;
     private JTextField userInputField;
+
     MainConsolePanel(int rows, int columns) {
         super(new BorderLayout());
 
-        gameText = new JTextArea();
-        gameText.setRows(rows);
-        gameText.setColumns(columns);
+        gameText = new JTextPane();
+        gameText.setBackground(Color.BLACK);
+        gameText.setForeground(Color.GREEN);
         gameText.setEditable(false);
 
         userInputField = new JTextField();
@@ -26,19 +31,17 @@ class MainConsolePanel extends JPanel {
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        //Do Something
+                        //Handles user input
+                        //TODO: Implement when input handler created
                     }
                 }
         );
 
         header = new JLabel("Header");
 
-        add(header,BorderLayout.PAGE_START);
-        add(gameText,BorderLayout.CENTER);
-        add(userInputField,BorderLayout.PAGE_END);
+        add(header, BorderLayout.PAGE_START);
+        add(gameText, BorderLayout.CENTER);
+        add(userInputField, BorderLayout.PAGE_END);
     }
-    void adjustSize(int rows, int columns) {
-        gameText.setRows(rows);
-        gameText.setColumns(columns);
-    }
+
 }
