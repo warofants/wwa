99de1217dfd1e681ee90a6682fdc5a3ff9d70e34
Veradux <veradux@abv.bg>
2019-08-20 20:34:44 +0300

    Add WorldMock methods for the example

--- src/main/java/com/worldwarofants/game/model/World.java
+++ src/main/java/com/worldwarofants/game/model/World.java
@@ -1,9 +1,34 @@
 package com.worldwarofants.game.model;
 
+import java.util.List;
+
 /**
  * The "database".
  * TODO docs
  */
 public class World {
 
+    /**
+     * Method that will be mocked by WorldMock.
+     * TODO remove once architecture is approved.
+     */
+    public ExampleAnt findAntByName(String name) {
+        return null;
+    }
+
+    /**
+     * Method that will be mocked by WorldMock.
+     * TODO remove once architecture is approved.
+     */
+    public void addAnt(ExampleAnt child) {
+
+    }
+
+    /**
+     * Method that will be mocked by WorldMock.
+     * TODO remove once architecture is approved.
+     */
+    public List<ExampleAnt> getAllAnts() {
+        return null;
+    }
 }
--- src/main/java/com/worldwarofants/game/model/WorldMock.java
+++ src/main/java/com/worldwarofants/game/model/WorldMock.java
@@ -1,11 +1,15 @@
 package com.worldwarofants.game.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
  * This class is only to show how the architecture works.
- * And to show that mocking of the World is possible, therefore allowing unit testing.
+ * It also shows that mocking of the World is possible,
+ * therefore allowing unit testing on classes who use it.
+ *
+ * TODO remove once architecture is approved.
  */
 public class WorldMock extends World {
 
@@ -17,10 +21,24 @@ public class WorldMock extends World {
     }
 
     private List<ExampleAnt> initFirstAnts() {
-        ExampleAnt charlie = new ExampleAnt("Charlie");
-        ExampleAnt jenny = new ExampleAnt("Jennifer");
-        jenny.findNewMate(charlie);
-        return Arrays.asList(charlie, jenny);
+        ExampleAnt charlie = new ExampleAnt("charlie");
+        ExampleAnt eve = new ExampleAnt("eve");
+        return new ArrayList<>(Arrays.asList(charlie, eve));
     }
 
+    @Override
+    public ExampleAnt findAntByName(String name) {
+        return ants.stream()
+                .filter(ant -> name.equals(ant.getName()))
+                .findAny()
+                .orElse(null);
+    }
+
+    public void addAnt(ExampleAnt ant) {
+        ants.add(ant);
+    }
+
+    public List<ExampleAnt> getAllAnts() {
+        return ants;
+    }
 }
