a6b68722808fd17a4202cdffd103dd05f0592163
Veradux <veradux@abv.bg>
2019-08-21 10:17:22 +0300

    Add World docs

--- src/main/java/com/worldwarofants/game/model/World.java
+++ src/main/java/com/worldwarofants/game/model/World.java
@@ -3,21 +3,23 @@ package com.worldwarofants.game.model;
 import java.util.List;
 
 /**
- * The "database".
- * TODO docs
+ * <p>
+ * The World serves the role of the "database".
+ * It should contain all information about the game models.
+ * The World is what the Controllers interact with,
+ * and where they get the information to give to the views. </p>
+ * <p>
+ * If at one point this class starts to get too long,
+ * a new layer of the architecture will be added,
+ * to split up the queries in the World class.
+ * </p>
+ * @see com.worldwarofants.game.arch.AbstractController
+ * @see com.worldwarofants.game.arch.AbstractView
  */
 public class World {
 
     /**
-     * Method that will be mocked by WorldMock.
-     * TODO remove once architecture is approved.
-     */
-    public ExampleAnt findAntByName(String name) {
-        return null;
-    }
-
-    /**
-     * Method that will be mocked by WorldMock.
+     * Method that is mocked by WorldMock.
      * TODO remove once architecture is approved.
      */
     public void addAnt(ExampleAnt child) {
@@ -25,7 +27,15 @@ public class World {
     }
 
     /**
-     * Method that will be mocked by WorldMock.
+     * Method that is mocked by WorldMock.
+     * TODO remove once architecture is approved.
+     */
+    public ExampleAnt findAntByName(String name) {
+        return null;
+    }
+
+    /**
+     * Method that is mocked by WorldMock.
      * TODO remove once architecture is approved.
      */
     public List<ExampleAnt> getAllAnts() {
